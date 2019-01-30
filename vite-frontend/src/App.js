c9618250ced12dc1ea593609f06bc5729eb4eff3
jun097kim <jun097kim@gmail.com>
2019-01-29 22:41:44 -0800

    Add Vite.js

--- vite-frontend/src/App.js
+++ vite-frontend/src/App.js
@@ -1,6 +1,24 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import provider from "@vite/vitejs/dist/es5/provider/WS";
+import { client } from "@vite/vitejs";
 
 class App extends Component {
+  componentDidMount() {
+    const WS_RPC = new provider("ws://127.0.0.1:41420");
+    const myClient = new client(WS_RPC, _myClient => {
+      console.log("Connected");
+    });
+
+    myClient.ledger
+      .getSnapshotChainHeight()
+      .then(result => {
+        console.log(result);
+      })
+      .catch(err => {
+        console.warn(err);
+      });
+  }
+
   render() {
     return null;
   }
